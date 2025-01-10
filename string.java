e3e7df5c4b493fc9da4bd2700b79b1ba7a0a8bb1
janmejay25 <janmjaypandya@gmail.com>
2025-01-10 16:45:52 +0530

    string used

---
+++ string.java
@@ -0,0 +1,8 @@
+public class string {
+    public static void main(String[] args) {
+        String str = "hello world";
+        System.out.println("string is "+str);
+        System.out.println("string in upper case "+str.toUpperCase());
+
+    }
+}
