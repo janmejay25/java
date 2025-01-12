12194a72fc404d1cb3a2869e6dd803242c4a5d2e
janmejay25 <janmjaypandya@gmail.com>
2025-01-12 19:33:57 +0530

    second method added

--- pattern.java
+++ pattern.java
@@ -6,6 +6,10 @@ public class pattern {
         System.out.println("enter string ");
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
+        //second method(copilot method)
+        // for (int i = 1; i <= s.length(); i++) {
+        //     System.out.println(s.substring(0, i));
+        // }
         for(int i=0;i<s.length();i++){
             for(int j = 0;j<=i;j++){
                 System.out.print(" "+s.charAt(j));
@@ -23,3 +27,7 @@ public class pattern {
 //  p y t h
 //  p y t h o
 //  p y t h o n
+
+//second method
+
+
