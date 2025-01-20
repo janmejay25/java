ba30c205b58f1ad637396883f1519ca470193fb4
janmejay25 <janmjaypandya@gmail.com>
2025-01-20 18:16:41 +0530

    M string builder

--- rev_string.java
+++ rev_string.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class rev_string {
     public static void main(String[] args) {
-        String rev = "";
+        StringBuilder rev = new StringBuilder();
         System.out.println("enter the string");
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         int len = s.length();
         for(int i = len-1;i>=0;i--){
-            rev = rev + s.charAt(i);
+            rev.append(s.charAt(i));
         }
         System.out.println("reverse string is "+rev);
-        if(s.equals(rev)){
+        if(s.contentEquals(rev)){
             System.out.println("string is palindrome");
         }
         else{
