d1842c5bc86806e2f462f7a6ad7b6d5be1b881d6
janmejay25 <janmjaypandya@gmail.com>
2025-01-27 12:22:20 +0530

    multilevel inheritence

---
+++ multilevel.java
@@ -0,0 +1,32 @@
+class x{
+    public void myM1(){
+        System.out.println("hello");
+    }
+}
+class y extends x{
+    public void myM2(){
+        System.out.println("good morning");
+    }
+}
+class z extends y{
+    public void myM3(){
+        System.out.println("how are you");
+    }
+}
+public class multilevel{
+    public static void main(String[] args){
+        z obj = new z();
+        obj.myM1();
+        obj.myM2();
+        obj.myM3();
+
+    }
+}
+// ```
+// Output:
+// ```
+// hello
+// good morning
+// how are you
+// ```
+
