738fe6952ad73e5ff2365a5f336fee7cbf6e4aaa
janmejay25 <janmjaypandya@gmail.com>
2025-02-01 20:16:07 +0530

    while cont break

--- WhileLoop.java
+++ WhileLoop.java
@@ -1,2 +1,16 @@
 public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        while (true) {
+            if (i == 3) {
+                i++;
+                continue; // Skip the rest of the loop when i is 3
+            }
+            System.out.println("Hello World " + i);
+            i++;
+            if (i == 5) {
+                break; // Exit the loop when i is 5
+            }
+        }
+    }
 }
