65caa4cc7abeaae8a434a0dfff071f5d07e83df4
janmejay25 <janmjaypandya@gmail.com>
2025-02-08 18:44:43 +0530

    basic function

---
+++ functionexample.java
@@ -0,0 +1,10 @@
+public class functionexample{
+    public static int add(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        int result = add(2,5);
+        System.out.println("addition is "+result); 
+        
+    }
+}
