6855e7ce4caa23588d55c3611d441c77bf98a1fe
janmejay25 <janmjaypandya@gmail.com>
2025-02-24 21:51:53 +0530

    package made userDefined:

---
+++ package/mypackage/MyClass.java
@@ -0,0 +1,11 @@
+package mypackage;
+
+public class MyClass {
+    public class myclass() {
+    public void mymethod() {
+            System.out.println("This is a method of myClass");
+    }
+    public int add(int a, int b) {
+        return a+b;
+    }
+}
---
+++ package/mypackage/main.java
@@ -0,0 +1,11 @@
+
+import mypackage;
+
+public class main {
+    public static void main(String[] args) {
+        MyClass obj = new MyClass();
+        obj.mymethod();
+        System.out.println(obj.add(10, 20));
+    }
+}
+
