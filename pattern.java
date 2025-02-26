9bb20b3f7b0c9aad4713166810c1aa46cfafb8c1
janmejay25 <janmjaypandya@gmail.com>
2025-02-26 10:25:23 +0530

    even odd pattern

--- pattern.java
+++ pattern.java
@@ -10,11 +10,29 @@ public class pattern {
         // for (int i = 1; i <= s.length(); i++) {
         //     System.out.println(s.substring(0, i));
         // }
-        for(int i=0;i<s.length();i++){
-            for(int j = 0;j<=i;j++){
-                System.out.print(" "+s.charAt(j));
+
+
+        // for(int i=0;i<s.length();i++){
+        //     for(int j = 0;j<=i;j++){
+        //         System.out.print(" "+s.charAt(j));
+        //     }
+        //     System.out.println(" ");
+        // }
+
+
+
+        int even = 2;
+        int odd = 1;
+        for(int i = 1;i<=6;i++){
+            for(int j = 1;j<=i;j++){
+                if(i%2==1){System.out.println(odd+"");
+                odd+=2;}
+                else if(i%2==0){
+                    System.out.println(even+"");
+                    even+=2;
+                }
             }
-            System.out.println(" ");
+//            System.out.println();
         }
     }
 }
@@ -29,5 +47,8 @@ public class pattern {
 //  p y t h o n
 
 //second method
+
+
+
 
 
