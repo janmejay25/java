0c624f60542efc13bbcc67b135c281342296bb0c
janmejay25 <janmjaypandya@gmail.com>
2025-04-29 09:33:30 +0530

    type caste

--- inc.java
+++ inc.java
@@ -5,6 +5,14 @@ class logical{
     boolean result =(x>4)||(y<11);
     boolean not2 = !(x>4);
 }
+class typecast{
+    double a = 5.502;
+    int b = (int) a;
+
+    //implicit type casting
+    int c = 5;
+    double d = c;
+}
 class inc{
     public static void main(String[] args){
         int a;
@@ -22,5 +30,13 @@ class inc{
         logical not = new logical();
         System.out.println(not.result); // 5
         System.out.println(not.not2); // false
+
+
+
+        typecast tc = new typecast();
+        System.out.println(tc.a); // 5.502
+        System.out.println(tc.b); // 5
+        System.out.println(tc.c); // 5
+        System.out.println(tc.d); // 5.0
     }
 }
