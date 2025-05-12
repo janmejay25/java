eb1d581992728d70a5d1899cd389247c6342d163
janmejay25 <janmjaypandya@gmail.com>
2025-05-12 22:04:52 +0530

    practice

---
+++ Demo.java
@@ -0,0 +1,29 @@
+class Shape {
+    void draw() {
+        System.out.println("Drawing a shape");
+    }
+}
+
+class Circle extends Shape {
+    void draw() {
+        System.out.println("Drawing a circle");
+    }
+}
+
+class Rectangle extends Shape {
+    void draw() {
+        System.out.println("Drawing a rectangle");
+    }
+}
+
+public class Demo {
+    public static void main(String[] args) {
+        Shape s;
+
+        s = new Circle();
+        s.draw();  // Output: Drawing a circle
+
+        s = new Rectangle();
+        s.draw();  // Output: Drawing a rectangle
+    }
+}
